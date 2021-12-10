f364d6dea9d6072e72dcce9123995688eefb70c9
Aaron Granick <aaron.granick@okta.com>
2021-12-10 14:22:42 -0800

    use jasmine framework in wdio

--- samples/test/wdio.conf.ts
+++ samples/test/wdio.conf.ts
@@ -172,7 +172,7 @@ export const config: WebdriverIO.Config = {
     //
     // Make sure you have the wdio adapter package for the specific framework installed
     // before running any tests.
-    framework: 'mocha',
+    framework: 'jasmine',
     //
     // The number of times to retry the entire specfile when it fails as a whole
     // specFileRetries: 1,
